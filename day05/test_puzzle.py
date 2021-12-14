443d29a6e2227ac64c1758cd293aea8e9767e3ad
Valerio Maggio <leriomaggio@users.noreply.github.com>
2021-12-14 00:24:30 +0000

    Day05: Hydrothermal Venture

---
+++ day05/test_puzzle.py
@@ -0,0 +1,63 @@
+"""Test Module for Puzzles in Day 05: Hydrothermal Venture"""
+
+import logging
+from pathlib import Path
+from pytest import fixture
+
+from puzzle import load, part1, part2
+from puzzle import __day__, __title__
+
+LOGGER = logging.getLogger(__name__)
+
+EXAMPLE_DATA = """0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+"""
+
+LOGGER.warning(f"Test AoC21 Day {__day__}: {__title__}")
+
+
+class AoCTest:
+    @fixture(scope="function")
+    def sample_input(self) -> list[int]:
+        LOGGER.info("Test Input")
+        return load(EXAMPLE_DATA.splitlines())
+
+    @fixture(scope="function")
+    def game_input(self) -> list[int]:
+        LOGGER.info("Game Input")
+        input_filepath = Path(__file__).with_name("input.05")
+        return load(open(input_filepath).read().strip().splitlines())
+
+
+# ------ Part 1 ------
+
+
+class TestPartOne(AoCTest):
+    def test_part1_on_sample_data(self, sample_input):
+        LOGGER.info(f"Part 1: Test Input")
+        assert part1(sample_input) == 5, f"Part 1 - Test Input ❌"
+
+    def test_part1_on_game_input(self, game_input):
+        LOGGER.info(f"Part 1: Game Input")
+        assert part1(game_input) == 4728, f"Part 1 - Game Input ❌"
+
+
+# ----- Part 2 -----
+
+
+class TestPartTwo(AoCTest):
+    def test_part2_on_sample_data(self, sample_input):
+        LOGGER.info(f"Part 2: Test Input")
+        assert part2(sample_input) == 12, f"Part 2 - Test Input ❌"
+
+    def test_part2_on_game_input(self, game_input):
+        LOGGER.info(f"Part 2: Game Input")
+        assert part2(game_input) == 17717, f"Part 2 - Game Input ❌"
