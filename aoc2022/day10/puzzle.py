a4eb018832518e25ae8c2252f9d65f69e932670e
Valerio Maggio <leriomaggio@users.noreply.github.com>
2022-12-10 11:52:21 +0000

    Day10: Cathode-Ray Tube

---
+++ aoc2022/day10/puzzle.py
@@ -0,0 +1,124 @@
+""" -- Advent of Code 2022 --
+Day XX, https://adventofcode.com/2022/day/10
+
+Notes on Solutions:
+Having fun with Python generators.
+The core of the solution relies on a CPU class implementation 
+which reads from an infinite tape.
+
+Every time a new instruction is read from the tape, the appropriate
+`op` is called (thanks to Python `getattr`).
+
+The `noop` operation, just ticks the clock and returns.
+The `addx` operation ticks (i.e. yields) the clock twice while also
+properly adding the value to X.
+
+The timing of operations is automatically managed via the yield
+operators, along with self contained (private, i.e. _xx) methods that
+handle the ops atomically.
+In more details:
+- _draw manages the on screen operations
+- _tick manages the clock tick.
+
+Public OPs (`noop` and `addx`) leverages on atomic internal ops
+to handle clock
+
+The solutions simply consume the generator for each data entry, which 
+automatically syncs with internal clock ticking.
+"""
+
+__day__ = "10"
+__title__ = "Cathode-Ray Tube"
+__author__ = "leriomaggio"
+
+from typing import Union
+from pathlib import Path
+
+
+def load(filepath: Union[str, Path]) -> list[str]:
+    return [line for line in open(filepath).read().split("\n")]
+
+
+class CPU:
+
+    SCREEN_ROWS = 40
+
+    def __init__(self) -> None:
+        self.clock = 1
+        self.sprite = 1
+        self.screen = ""
+        self.signal_strength = 0
+
+    def read(self):
+        line = None
+        while True:
+            instruction = yield line
+            op, *value = instruction.split()
+            for tick in getattr(self, op)(value):
+                yield tick
+                if self.clock % self.SCREEN_ROWS == (self.SCREEN_ROWS / 2):
+                    self.signal_strength += self.clock * self.sprite
+
+    def _draw(self):
+        position = (self.clock - 1) % self.SCREEN_ROWS
+        self.screen += (
+            "#" if position in range(self.sprite - 1, self.sprite + 2) else "."
+        )
+        if position == self.SCREEN_ROWS - 1:
+            self.screen += "\n"
+
+    def _tick(self):
+        self._draw()
+        self.clock += 1
+
+    def noop(self, _):
+        self._tick()
+        yield self.clock
+
+    def addx(self, values):
+        self._tick()
+        yield self.clock
+        self._tick()
+        self.sprite += int(values[0])
+        yield self.clock
+
+
+# =========== Part 1 ============
+
+
+def part1(data: list[str]) -> int:
+    cpu = CPU()
+    tape = cpu.read()
+    next(tape)
+    for line in data:
+        tick = tape.send(line)
+        while next(tape):
+            pass  # noop
+    return cpu.signal_strength
+
+
+# =========== Part 2 ============
+
+
+def part2(data: list[int]) -> int:
+    cpu = CPU()
+    tape = cpu.read()
+    next(tape)
+    for line in data:
+        tick = tape.send(line)
+        while next(tape):
+            pass  # noop
+    print(cpu.screen)
+    return cpu.signal_strength  # only for tests to pass
+
+
+if __name__ == "__main__":
+    print("=*=*=*=*=*=*=*=*=*= Advent of Code 2022 =*=*=*=*=*=*=*=*=*=")
+    print(f"Day {__day__}: {__title__}")
+    print("-" * 59)
+    filepath = Path(__file__).with_name(f"input.{__day__}")
+    data = load(filepath=filepath)
+    # solve part 1
+    print(part1(data))
+    # solve part 2
+    print(part2(data))
---
+++ aoc2022/day10/test_puzzle.py
@@ -0,0 +1,198 @@
+"""Test Module for Puzzles in Day 10: Cathode-Ray Tube"""
+
+import logging
+from pathlib import Path
+from pytest import fixture, mark
+
+from puzzle import load, part1, part2
+from puzzle import __day__, __title__
+
+LOGGER = logging.getLogger(__name__)
+
+EXAMPLE_DATA = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""
+
+LOGGER.warning(f"Test AoC22 Day {__day__}: {__title__}")
+
+
+class AoCTest:
+    @fixture(scope="function")
+    def sample_input(self) -> list[int]:
+        LOGGER.info("Test Input")
+        return EXAMPLE_DATA.splitlines()
+
+    @fixture(scope="function")
+    def game_input(self) -> list[int]:
+        LOGGER.info("Game Input")
+        return load(filepath=Path(__file__).with_name(f"input.{__day__}"))
+
+
+# ------ Part 1 ------
+
+
+class TestPartOne(AoCTest):
+    def test_part1_on_sample_data(self, sample_input):
+        LOGGER.info(f"Part 1: Test Input")
+        assert part1(sample_input) == 13140, f"Part 1 - Test Input ❌"
+
+    def test_part1_on_game_input(self, game_input):
+        LOGGER.info(f"Part 1: Game Input")
+        assert part1(game_input) == 13180, f"Part 1 - Game Input ❌"
+
+
+# ----- Part 2 -----
+
+
+class TestPartTwo(AoCTest):
+    def test_part2_on_sample_data(self, sample_input):
+        LOGGER.info(f"Part 2: Test Input")
+        assert part2(sample_input) == 13140, f"Part 2 - Test Input ❌"
+
+    def test_part2_on_game_input(self, game_input):
+        LOGGER.info(f"Part 2: Game Input")
+        assert part2(game_input) == 13180, f"Part 2 - Game Input ❌"
