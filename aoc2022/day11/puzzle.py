edc9835e26b5931119ba9aaeb415dcb240060b5e
Valerio Maggio <leriomaggio@users.noreply.github.com>
2022-12-11 09:19:03 +0000

    Day11: Monkey in the Middle

---
+++ aoc2022/day11/puzzle.py
@@ -0,0 +1,107 @@
+""" -- Advent of Code 2022 --
+Day 11, https://adventofcode.com/2022/day/11
+
+Notes on Solutions:
+- Part 1: This is quite straightforward, and does not require any particular adjustment
+in the first attempt.
+- Part 2: The solution to this part generalises the whole game by realising one important thing: the whole game with monkeys is based on divisibility of items.
+Since we have no `relief_factor` in this part, the **only** possible consideration that
+we can derive to avoid our stress level going to the roof is that worry level of items
+could never go beyon the product of all divisors.
+"""
+
+__day__ = "11"
+__title__ = "Monkey in the Middle"
+__author__ = "leriomaggio"
+
+from typing import Union
+from pathlib import Path
+from math import prod
+from operator import sub, add, truediv, mul
+from typing import Optional
+from collections import deque, namedtuple
+
+operations = {"+": add, "-": sub, "*": mul, "/": truediv}
+Item = namedtuple("Item", ["to", "item"])
+
+
+class Monkey:
+    def __init__(self, monkey_card: str) -> None:
+        m_info = monkey_card.split("\n")
+        self.items = deque(list(map(int, m_info[1].split(":")[1].strip().split(","))))
+        self.divisible_by = int(m_info[3].split("divisible by")[1].strip())
+        self._worry_expr = m_info[2].split("=")[1].strip()
+        self._mates = [
+            int(l.split("throw to monkey")[1].strip()) for l in (m_info[5], m_info[4])
+        ]
+        self.inspected = 0
+
+    def play(self, relief_factor: int, prod_divisors: int) -> Optional[Item]:
+        if not self.items:
+            return None
+        item = (self.worry_level(self.items.popleft()) // relief_factor) % prod_divisors
+        self.inspected += 1
+        return Item(item=item, to=self._mates[int((item % self.divisible_by == 0))])
+
+    def worry_level(self, item):
+        l_operand, operator, r_operand = self._worry_expr.split()
+        l_operand = item if l_operand == "old" else int(l_operand)
+        r_operand = item if r_operand == "old" else int(r_operand)
+        return operations[operator](l_operand, r_operand)
+
+    def catch_item(self, item):
+        self.items.append(item)
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __str__(self) -> str:
+        return f"Monkey: {self.items}; {self._worry_expr}; {self.divisible_by}; {self._mates}"
+
+
+def load(filepath: Union[str, Path]) -> list[Monkey]:
+    return parse(open(filepath).read())
+
+
+def parse(data: str) -> list[Monkey]:
+    return [Monkey(m_card) for m_card in data.split("\n\n")]
+
+
+def game(monkeys: list[Monkey], n_rounds: int, relief: int) -> int:
+    prod_divisors = prod([monkey.divisible_by for monkey in monkeys])
+    for r in range(n_rounds):
+        for monkey in monkeys:
+            while True:
+                thrown_item = monkey.play(
+                    prod_divisors=prod_divisors, relief_factor=relief
+                )
+                if thrown_item is None:
+                    break
+                monkeys[thrown_item.to].catch_item(thrown_item.item)
+    return prod(sorted([m.inspected for m in monkeys], reverse=True)[:2])
+
+
+# =========== Part 1 ============
+
+
+def part1(data: list[Monkey]) -> int:
+    return game(data, n_rounds=20, relief=3)
+
+
+# =========== Part 2 ============
+
+
+def part2(data: list[Monkey]) -> int:
+    return game(data, n_rounds=10000, relief=1)
+
+
+if __name__ == "__main__":
+    print("=*=*=*=*=*=*=*=*=*= Advent of Code 2022 =*=*=*=*=*=*=*=*=*=")
+    print(f"Day {__day__}: {__title__}")
+    print("-" * 59)
+    filepath = Path(__file__).with_name(f"input.{__day__}")
+    data = load(filepath=filepath)
+    # solve part 1
+    print(part1(data))
+    # solve part 2
+    print(part2(data))
---
+++ aoc2022/day11/test_puzzle.py
@@ -0,0 +1,79 @@
+"""Test Module for Puzzles in Day 11: Monkey in the Middle"""
+
+import logging
+from pathlib import Path
+from pytest import fixture, mark
+
+from puzzle import load, parse, part1, part2
+from puzzle import __day__, __title__
+
+LOGGER = logging.getLogger(__name__)
+
+EXAMPLE_DATA = """Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+"""
+
+LOGGER.warning(f"Test AoC22 Day {__day__}: {__title__}")
+
+
+class AoCTest:
+    @fixture(scope="function")
+    def sample_input(self) -> list[int]:
+        LOGGER.info("Test Input")
+        return parse(EXAMPLE_DATA)
+
+    @fixture(scope="function")
+    def game_input(self) -> list[int]:
+        LOGGER.info("Game Input")
+        return load(filepath=Path(__file__).with_name(f"input.{__day__}"))
+
+
+# ------ Part 1 ------
+
+
+class TestPartOne(AoCTest):
+    def test_part1_on_sample_data(self, sample_input):
+        LOGGER.info(f"Part 1: Test Input")
+        assert part1(sample_input) == 10605, f"Part 1 - Test Input ❌"
+
+    def test_part1_on_game_input(self, game_input):
+        LOGGER.info(f"Part 1: Game Input")
+        assert part1(game_input) == 117624, f"Part 1 - Game Input ❌"
+
+
+# ----- Part 2 -----
+
+
+class TestPartTwo(AoCTest):
+    def test_part2_on_sample_data(self, sample_input):
+        LOGGER.info(f"Part 2: Test Input")
+        assert part2(sample_input) == 2713310158, f"Part 2 - Test Input ❌"
+
+    def test_part2_on_game_input(self, game_input):
+        LOGGER.info(f"Part 2: Game Input")
+        assert part2(game_input) == 16792940265, f"Part 2 - Game Input ❌"
