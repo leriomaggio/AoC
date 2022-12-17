aa1d04862fb6f26a1552327369e91853745ce532
Valerio Maggio <leriomaggio@users.noreply.github.com>
2022-12-17 11:24:54 +0000

    Day 17: Pyroclastic Flow

---
+++ aoc2022/day17/puzzle.py
@@ -0,0 +1,170 @@
+""" -- Advent of Code 2022 --
+Day 17, https://adventofcode.com/2022/day/XX
+
+Notes on Solutions:
+The game is cyclic and repetitive (i.e. `itertools.cycle`),
+therefore the key to unlock both Part1 (but mostly) Part 2
+is to understand how to properly cache (dump) game states
+so that it's possible to restore the game configuration to
+that state.
+
+The idea of caching is indeed quite straightfoward (once you can
+see it 🙃): it would be necessary to store the corresponding
+ids of the current piece and the current direction of the wind
+(i.e. pattern), along with the configuration of the ceiling!.
+
+If that configuration has been already calculated, the game
+state (i.e. resting) could be restored so that each piece
+will have coordinates which is proportional to the "current"
+height AND number of pieces.
+
+Similarly, the current number of pieces on the screen are rescaled
+in a similar fashion, modulo the delta (current - cached).
+
+With this trick in mind, part1 and 2 could be solved with the
+same functions/methods.
+
+Relevant tricks used in this puzzle:
+- 2D coordinates as complex numbers (life savers!)
+- rocks as sets so that we could leverage on & (intersection)
+and | (union) for set operations.
+"""
+
+__day__ = "17"
+__title__ = "Pyroclastic Flow"
+__author__ = "leriomaggio"
+
+from typing import Union
+from pathlib import Path
+from itertools import cycle
+
+
+def load(filepath: Union[str, Path]):
+    return parse_input(open(filepath).read())
+
+
+def parse_input(data: str) -> str:
+    return data.strip()  # noop
+
+
+class Grid:
+    def __init__(self):
+        self._resting = set(x - 1j for x in range(7))
+        self._cache = dict()
+
+    @property
+    def height(self) -> int:
+        return max(int(r.imag) for r in self.resting)
+
+    @property
+    def ceiling(self) -> tuple[int]:
+        return tuple(
+            self.height - max(int(r.imag) for r in self.resting if r.real == i)
+            for i in range(7)
+        )
+
+    @property
+    def floor(self) -> complex:
+        return 2 + (4 + self.height) * 1j
+
+    @property
+    def resting(self) -> set[complex]:
+        return self._resting
+
+    @resting.setter
+    def resting(self, restings: set[complex]):
+        self._resting = restings
+
+    def __iter__(self):
+        for p in self.resting:
+            yield p
+
+    def __contains__(self, rock: set[complex]) -> bool:
+        return len(self._resting & rock)
+
+
+class Tetris:
+    PIECES = [  # (x, y), top to bottom
+        # ➖
+        {0 + 0j, 1 + 0j, 2 + 0j, 3 + 0j},
+        # ➕
+        {2 + 1j, 1 + 1j, 0 + 1j, 1 + 0j, 1 + 2j},
+        # L
+        {2 + 2j, 2 + 1j, 0 + 0j, 1 + 0j, 2 + 0j},
+        # |
+        {0 + 3j, 0 + 2j, 0 + 1j, 0 + 0j},
+        # 🟫
+        {1 + 0j, 1 + 1j, 0 + 0j, 0 + 1j},
+    ]
+
+    DIRECTIONS = {">": 1, "<": -1}
+
+    def __init__(self, winds: str, n_rocks: int) -> None:
+        self.n_rocks = n_rocks
+        self.rocks = cycle(enumerate(self.PIECES))
+        self.winds = cycle(enumerate(self.DIRECTIONS[d] for d in winds))
+        self.grid = Grid()
+        self.r_counter = n_rocks
+        self.cache = dict()
+
+    def move(self, rock: set[complex]):
+        rock = {self.grid.floor + p for p in rock}
+        while True:
+            wind_idx, w = next(self.winds)
+            w_rock = {r + w for r in rock}
+            if all(0 <= r.real <= 6 for r in w_rock) and not w_rock in self.grid:
+                rock = w_rock
+            w_rock = {p - 1j for p in rock}  # move downwards
+            if w_rock in self.grid:
+                break
+            rock = w_rock
+        self.grid.resting |= rock
+        return wind_idx
+
+    def simulate(self):
+
+        while self.r_counter > 0:
+            rock_idx, rock = next(self.rocks)
+            wind_idx = self.move(rock)
+            self.r_counter -= 1
+            cache_key = (rock_idx, wind_idx, self.grid.ceiling)
+            r_counter_cache, cached_height = self.cache.setdefault(
+                cache_key,
+                (self.r_counter, self.grid.height),
+            )
+            if r_counter_cache != self.r_counter:
+                delta_t, delta_y = (r_counter_cache - self.r_counter), (
+                    self.grid.height - cached_height
+                )
+                self.grid.resting = {
+                    r + (self.r_counter // delta_t) * (delta_y * 1j) for r in self.grid
+                }
+                self.r_counter %= delta_t
+
+        return self.grid.height + 1
+
+
+# =========== Part 1 ============
+
+
+def part1(data: list[int]) -> int:
+    return Tetris(winds=data, n_rocks=2022).simulate()
+
+
+# =========== Part 2 ============
+
+
+def part2(data: list[int]) -> int:
+    return Tetris(winds=data, n_rocks=1000000000000).simulate()
+
+
+if __name__ == "__main__":
+    print("=*=*=*=*=*=*=*=*=*= Advent of Code 2022 =*=*=*=*=*=*=*=*=*=")
+    print(f"Day {__day__}: {__title__}")
+    print("-" * 59)
+    filepath = Path(__file__).with_name(f"input.{__day__}")
+    data = load(filepath=filepath)
+    # solve part 1
+    print(part1(data))
+    # solve part 2
+    print(part2(data))
---
+++ aoc2022/day17/test_puzzle.py
@@ -0,0 +1,52 @@
+"""Test Module for Puzzles in Day 17: Pyroclastic Flow"""
+
+import logging
+from pathlib import Path
+from pytest import fixture, mark
+
+from puzzle import load, parse_input, part1, part2
+from puzzle import __day__, __title__
+
+LOGGER = logging.getLogger(__name__)
+
+EXAMPLE_DATA = """>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"""
+
+LOGGER.warning(f"Test AoC22 Day {__day__}: {__title__}")
+
+
+class AoCTest:
+    @fixture(scope="function")
+    def sample_input(self) -> list[int]:
+        LOGGER.info("Test Input")
+        return parse_input(EXAMPLE_DATA.strip())
+
+    @fixture(scope="function")
+    def game_input(self) -> list[int]:
+        LOGGER.info("Game Input")
+        return load(filepath=Path(__file__).with_name(f"input.{__day__}"))
+
+
+# ------ Part 1 ------
+
+
+class TestPartOne(AoCTest):
+    def test_part1_on_sample_data(self, sample_input):
+        LOGGER.info(f"Part 1: Test Input")
+        assert part1(sample_input) == 3068, f"Part 1 - Test Input ❌"
+
+    def test_part1_on_game_input(self, game_input):
+        LOGGER.info(f"Part 1: Game Input")
+        assert part1(game_input) == 3055, f"Part 1 - Game Input ❌"
+
+
+# ----- Part 2 -----
+
+
+class TestPartTwo(AoCTest):
+    def test_part2_on_sample_data(self, sample_input):
+        LOGGER.info(f"Part 2: Test Input")
+        assert part2(sample_input) == 1514285714288, f"Part 2 - Test Input ❌"
+
+    def test_part2_on_game_input(self, game_input):
+        LOGGER.info(f"Part 2: Game Input")
+        assert part2(game_input) == 1507692307690, f"Part 2 - Game Input ❌"
